7a09711554f4afc3ed652a8c6b874370400129d5
r4ng3l <rangel93@protonmail.com>
2024-10-30 13:54:26 -0600

    Update libraries (lifecicle-runtime-ktx & compose-bom)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    //implementation(libs.androidx.material.icons.extended)
+    implementation(libs.androidx.material.icons.extended)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.retrofit)
     implementation(libs.retrofit.converter.gson)
